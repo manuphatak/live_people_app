69a33cc9f9f5b836784409da34ee7a0f0e90fa3c
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-08-20 13:25:47 -0500

    setup model bindings and sync channel

--- config/routing.py
+++ config/routing.py
@@ -1,2 +1,10 @@
 # coding=utf-8
-channel_routing = []
+from channels import route, route_class
+
+from live_people_app.people.consumers import AppDemultiplexer, PersonBinding, SyncConsumer
+
+channel_routing = [
+    route_class(AppDemultiplexer),
+    route('binding.Person', PersonBinding.consumer),
+    route('consumer.Sync', SyncConsumer.consumer),
+]
---
+++ live_people_app/people/consumers.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+from channels.binding.websockets import WebsocketBinding
+from channels.generic.websockets import WebsocketDemultiplexer
+
+from .models import Person
+from ..utils.consumers import ReplyChannelConsumer
+
+
+class AppDemultiplexer(WebsocketDemultiplexer):
+    mapping = {
+        'Person': 'binding.Person',
+        'Sync': 'consumer.Sync',
+    }
+
+    def connection_groups(self, **kwargs):
+        return ['App']
+
+
+class PersonBinding(WebsocketBinding):
+    stream = 'Person'
+    model = Person
+    fields = ['first_name', 'last_name', 'dob', 'zip', 'created']
+
+    def group_names(self, instance, action):
+        return ['App']
+
+    def has_permission(self, user, action, pk):
+        return True
+
+
+class SyncConsumer(ReplyChannelConsumer):
+    model = Person
+    stream = 'Sync'
+    fields = ['first_name', 'last_name', 'dob', 'zip', 'created']
+    actions = ['details', 'list']
+
+    def has_permission(self, user, action, pk):
+        return True
+
+    def details(self):
+        return self.queryset.get(pk=self.pk)
+
+    def list(self):
+        return self.queryset
