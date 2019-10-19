2b5346d51b7803205627f767658d18184250014d
Manu Phatak <manu.phatak@procore.com>
2019-10-19 15:51:38 -0700

    prune dependencies

--- config/settings/local.py
+++ config/settings/local.py
@@ -45,24 +45,6 @@ CACHES = {
     }
 }
 
-# # django-debug-toolbar
-# # ------------------------------------------------------------------------------
-# MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-# INSTALLED_APPS += ('debug_toolbar',)
-
-# INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', ]
-# # tricks to have debug toolbar when developing with docker
-# if os.environ.get('USE_DOCKER') == 'yes':
-#     ip = socket.gethostbyname(socket.gethostname())
-#     INTERNAL_IPS += [ip[:-1] + "1"]
-
-# DEBUG_TOOLBAR_CONFIG = {
-#     'DISABLE_PANELS': [
-#         'debug_toolbar.panels.redirects.RedirectsPanel',
-#     ],
-#     'SHOW_TEMPLATE_CONTEXT': True,
-# }
-
 # django-extensions
 # ------------------------------------------------------------------------------
 INSTALLED_APPS += ('django_extensions',)
